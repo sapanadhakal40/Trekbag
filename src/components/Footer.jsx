0b423d4223c3cd479830a74f3cdf27aae2e34b3b
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-30 00:27:26 +0300

    Add basic components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <Footer></Footer>;
+  return (
+    <footer>
+      <small>&copy; 2080. Copyright by Noone</small>
+      <p>
+        Version <b></b>1.5
+      </p>
+    </footer>
+  );
 }
