267b564b4cd3c65f0f5974620a4b0a33b8416e1a
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-30 00:01:20 +0300

    Initial Vite setup with file structure and CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import BackgroundHeading from "./components/BackgroundHeading";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <BackgroundHeading />
+      <main></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
