6e5781813269194c0dd34b950f7237a8de89f229
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-30 01:56:40 +0300

    Add form component

---
+++ src/components/AddItemForm.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button";
+
+export default function AddItemForm() {
+  return (
+    <form>
+      <h2>Add an item</h2>
+      <input type="text" required />
+      <Button>Add Item</Button>
+    </form>
+  );
+}
