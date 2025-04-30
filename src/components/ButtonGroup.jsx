367db790852540e43a4afd075e8e8d966f331538
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-30 09:51:31 +0300

    Update secondary buttons with mapping

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,22 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Removes all items",
+];
 export default function ButtonGroup() {
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Removes all items</Button>
+      {secondaryButtons.map((text) => {
+        return (
+          <Button key={text} type="secondary">
+            {text}
+          </Button>
+        );
+      })}
+      {}
     </section>
   );
 }
