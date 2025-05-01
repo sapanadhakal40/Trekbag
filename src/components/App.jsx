d682163ad951a1495f14e491af6a03f3229e4c6e
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-01 23:27:27 +0300

    Implement state lifting with prop(setItems)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,18 @@ import Footer from "./Footer";
 import ItemList from "./ItemList";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
+import { useState } from "react";
+import { initialItems } from "../lib/constants";
 
 function App() {
+  const [items, setItems] = useState(initialItems);
   return (
     <>
       <BackgroundHeading />
       <main>
         <Header />
-        <ItemList />
-        <Sidebar />
+        <ItemList items={items} />
+        <Sidebar setItems={setItems} />
       </main>
       <Footer />
     </>
