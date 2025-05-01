709f9341dac6d584986e22c21acb3e5d90515e0e
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-02 01:29:17 +0300

    Pass handleAddItem function as props

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 import Button from "./Button";
 
-export default function AddItemForm({ setItems }) {
+export default function AddItemForm({ handleAddItem }) {
   const [itemText, setItemText] = useState("");
   const inputRef = useRef();
 
@@ -16,14 +16,7 @@ export default function AddItemForm({ setItems }) {
       return;
     }
 
-    const newItem = {
-      // id: 4,
-      id: new Date().getTime(),
-      name: itemText,
-      packed: false,
-    };
-
-    setItems((prev) => [...prev, newItem]);
+    handleAddItem(itemText);
     setItemText("");
   };
 
