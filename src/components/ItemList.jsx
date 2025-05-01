979e5078366f00436653a83802ca459b5222cc05
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-01 18:20:11 +0300

    Update items with mapped array

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,27 @@
+import { initialItems } from "../lib/constants";
+import { useState } from "react";
+
 export default function ItemList() {
-  return <div>Item List</div>;
+  const [items, setItems] = useState(initialItems);
+  return (
+    <ul>
+      {items.map((item) => {
+        return <Item key={item.name} item={item} />;
+      })}
+      {/* <Item item={item1} />
+      <Item item={item2} />
+      <Item item={item3} /> */}
+    </ul>
+  );
+}
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input checked={item.packed} type="checkbox" />
+        {item.name}
+      </label>
+      <button>❌</button>
+    </li>
+  );
 }
