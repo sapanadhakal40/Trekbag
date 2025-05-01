75688009e282808e0a30d1a56fe5947abeed57d1
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-02 01:26:18 +0300

    Move newitem creation logic from AddItemForm to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,25 @@ import { initialItems } from "../lib/constants";
 
 function App() {
   const [items, setItems] = useState(initialItems);
+
+  const handleAddItem = (newItemText) => {
+    const newItem = {
+      // id: 4,
+      id: new Date().getTime(),
+      name: newItemText,
+      packed: false,
+    };
+
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
   return (
     <>
       <BackgroundHeading />
       <main>
         <Header />
         <ItemList items={items} />
-        <Sidebar setItems={setItems} />
+        <Sidebar handleAddItem={handleAddItem} />
       </main>
       <Footer />
     </>
