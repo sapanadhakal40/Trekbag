4ad820e40775bd07c463fcc485382a3b95ceca1b
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-01 23:31:09 +0300

    Add input validation, useRef for focus ,create new item with timestamp id, and set submission handler

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,16 +1,42 @@
+import { useRef, useState } from "react";
 import Button from "./Button";
-import { useState } from "react";
 
-export default function AddItemForm() {
+export default function AddItemForm({ setItems }) {
   const [itemText, setItemText] = useState("");
+  const inputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(itemText);
+
+    // basic validation
+    if (!itemText) {
+      alert("Item can't be empty");
+      inputRef.current.focus();
+      return;
+    }
+
+    const newItem = {
+      // id: 4,
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false,
+    };
+
+    setItems((prev) => [...prev, newItem]);
+    setItemText("");
+  };
+
   return (
-    <form onSubmit={() => {}}>
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
       <input
+        ref={inputRef}
         value={itemText}
         onChange={(e) => {
           setItemText(e.target.value);
         }}
+        autoFocus={true}
       />
       <Button>Add Item</Button>
     </form>
