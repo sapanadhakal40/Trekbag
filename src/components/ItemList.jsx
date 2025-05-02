9e8a3ecae9099b7cc9a31e4f5016138758163b79
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-02 15:51:35 +0300

    Add react-select components for sorting options and update items display based on sort selection

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,16 +1,52 @@
+import Select from "react-select";
 import EmptyView from "./EmptyView";
+import { useState } from "react";
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+  {
+    label: "Sort by packed",
+    value: "packed",
+  },
+  {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
 
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState("default");
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === "packed") {
+      return b.packed - a.packed;
+      //  for positive result if b packed is true and other is false
+    }
+    if (sortBy === "unpacked") {
+      return a.packed - b.packed;
+    }
+    return;
+  });
   return (
     <ul className="item-list">
       {items.length === 0 ? <EmptyView /> : null}
       {/* {items.length === 0 && <EmptyView />} */}
-
-      {items.map((item) => {
+      {items.length > 0 ? (
+        <section className="sorting">
+          <Select
+            onChange={(option) => setSortBy(option.value)}
+            defaultValue={sortingOptions[0]}
+            options={sortingOptions}
+          />
+        </section>
+      ) : null}
+      {sortedItems.map((item) => {
         return (
           <Item
             key={item.name}
