0df0001348fb74631c5ea267d530118f2ce79deb
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-02 18:19:47 +0300

    Create custom hook for ItemsContext

---
+++ src/lib/hooks.js
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import { ItemsContext } from "../context/ItemsContextProvider";
+
+export function useItemsContext() {
+  const context = useContext(ItemsContext);
+  return context;
+}
