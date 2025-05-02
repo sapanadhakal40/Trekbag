317b90009182a97d3b87475a3e6579ecdd1656da
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-02 18:33:24 +0300

    Add error handling to useItemsContext hook

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -3,5 +3,11 @@ import { ItemsContext } from "../context/ItemsContextProvider";
 
 export function useItemsContext() {
   const context = useContext(ItemsContext);
+
+  if (!context) {
+    throw new Error(
+      "useItemContext must be used within a ItemsContextProvider"
+    );
+  }
   return context;
 }
