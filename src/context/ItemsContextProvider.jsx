bf9a70ded4726c6f7e4712a6e5303d70b7087dd9
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-02 18:09:34 +0300

    Create items context provider to handle items state management

---
+++ src/context/ItemsContextProvider.jsx
@@ -0,0 +1,80 @@
+import { createContext, useEffect, useState } from "react";
+import { initialItems } from "../lib/constants";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ItemsContext = createContext();
+
+export default function ItemsContextProvider({ children }) {
+  const [items, setItems] = useState(() => {
+    return JSON.parse(localStorage.getItem("items")) || initialItems;
+  });
+
+  const handleAddItem = (newItemText) => {
+    const newItem = {
+      // id: 4,
+      id: new Date().getTime(),
+      name: newItemText,
+      packed: false,
+    };
+
+    const newItems = [...items, newItem];
+    setItems(newItems);
+  };
+
+  const handleDeleteItem = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  };
+  const handleToggleItem = (id) => {
+    const newItems = items.map((item) => {
+      if (item.id === id) {
+        return { ...item, packed: !item.packed };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
+  const handleRemoveAllItems = () => {
+    setItems([]);
+  };
+
+  const handleResetToInitial = () => {
+    setItems(initialItems);
+  };
+
+  const handleMarkAllAsComplete = () => {
+    const newItems = items.map((item) => {
+      return { ...item, packed: true };
+    });
+    setItems(newItems);
+  };
+
+  const handleMarkAllAsIncomplete = () => {
+    const newItems = items.map((item) => {
+      return { ...item, packed: false };
+    });
+    setItems(newItems);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <ItemsContext.provider
+      value={{
+        items,
+        handleAddItem,
+        handleDeleteItem,
+        handleToggleItem,
+        handleRemoveAllItems,
+        handleResetToInitial,
+        handleMarkAllAsComplete,
+        handleMarkAllAsIncomplete,
+      }}
+    >
+      {children}
+    </ItemsContext.provider>
+  );
+}
